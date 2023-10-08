e3d5ef9e509697c04dca1219287f1a5f7e0fe9d6
KCstark <kanishakc1@gmail.com>
2023-10-08 15:30:33 +0530

    working without roles for now.

--- hackathonNew/target/classes/static/script.js
+++ hackathonNew/target/classes/static/script.js
@@ -2,7 +2,7 @@ function submitForm() {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    // var role = document.getElementById("role").value;
+    // var role = document.getElementById("role").value; //working only without roles for now
 
     // Send data to the backend
     fetch('/hackathon/testing/process', {
